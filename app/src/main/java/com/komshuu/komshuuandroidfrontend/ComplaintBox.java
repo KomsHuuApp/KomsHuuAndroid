8cf242c36134311b8b261763a01d178c0130a460
irmakersevinc <irmakersevinc@gmail.com>
2019-01-26 00:13:57 +0300

    şikayet kutusu sisteminin temeli

---
+++ app/src/main/java/com/komshuu/komshuuandroidfrontend/ComplaintBox.java
@@ -0,0 +1,14 @@
+package com.komshuu.komshuuandroidfrontend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ComplaintBox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_box);
+    }
+
+}
