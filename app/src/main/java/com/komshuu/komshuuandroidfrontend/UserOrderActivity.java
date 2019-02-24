950e5789567072279ac0133978dd9695a4d555ea
Arda <ardaarslan064@gmail.com>
2019-02-24 19:21:28 +0300

    Spinner and extra dialogs added. User can Cancel their orders now.

--- app/src/main/java/com/komshuu/komshuuandroidfrontend/UserOrderActivity.java
+++ app/src/main/java/com/komshuu/komshuuandroidfrontend/UserOrderActivity.java
@@ -5,9 +5,14 @@ import android.os.StrictMode;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
@@ -20,11 +25,19 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class UserOrderActivity extends AppCompatActivity {
+public class UserOrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
     EditText siparisInput;
     Button submitButton;
+    Button deleteButton;
+    String text = "";
+    String text1 = "";
+    int check = 0;
+    int TamBugday = 0;
+    int Cavdar = 0;
+    int Kepekli = 0;
+    int BeyazEkmek = 0;
 
     String server_url = "https://enigmatic-atoll-89666.herokuapp.com/addOrder";
 
@@ -34,8 +47,13 @@ public class UserOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_order);
 
 
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.numbers,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
-        siparisInput = (EditText) findViewById(R.id.siparisInput);
+
         submitButton = (Button) findViewById(R.id.submitButton);
         RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -46,8 +64,8 @@ public class UserOrderActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(UserOrderActivity.this);
-
-                builder.setMessage("Siparisi Vermek istediginizden Emin misiniz?")
+                builder.setTitle("Siparis Ver");
+                builder.setMessage("Siparisi Vermek istediginizden Emin misiniz?" + "\n" + "Mevcut siparisiniz: " + "\n" + text)
                         .setPositiveButton("Evet", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -63,7 +81,7 @@ public class UserOrderActivity extends AppCompatActivity {
                                     httpCon.setRequestMethod("POST");
                                     httpCon.setRequestProperty("Content-Type","application/json");
                                     JSONObject eventObject = new JSONObject();
-                                    eventObject.put("orderType",siparisInput.getText().toString());
+                                    eventObject.put("orderType",text);
                                     eventObject.put("apartmentId", new Long(2));
                                     eventObject.put("orderDate", time);
                                     String json = eventObject.toString();
@@ -75,6 +93,15 @@ public class UserOrderActivity extends AppCompatActivity {
 
                                     int responseCode = httpCon.getResponseCode();
                                     System.out.println("response code: " + responseCode);
+                                    if (responseCode == 200) {
+                                        AlertDialog.Builder builder1 = new AlertDialog.Builder(UserOrderActivity.this);
+                                        builder1.setMessage("Siparisiniz Basarıyla iletilmistir");
+                                        builder1.setPositiveButton("Tamam", null);
+
+                                        AlertDialog alert2 = builder1.create();
+                                        alert2.show();
+
+                                    }
                                 } catch (ProtocolException e) {
                                     e.printStackTrace();
                                 } catch (IOException e) {
@@ -93,5 +120,64 @@ public class UserOrderActivity extends AppCompatActivity {
 
             }
         });
+
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final AlertDialog.Builder builder1 = new AlertDialog.Builder(UserOrderActivity.this);
+                builder1.setTitle("Siparis İptali");
+                builder1.setMessage("Siparisi iptal etmek istediginizden Emin misiniz?" + "\n" + "Mevcut siparisiniz: " + "\n" + text);
+                builder1.setCancelable(true)
+                        .setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                text = "";
+                                AlertDialog.Builder builder2 = new AlertDialog.Builder(UserOrderActivity.this);
+                                builder2.setMessage("Siparisiniz iptal edilmistir");
+                                builder2.setPositiveButton("Tamam", null);
+
+                                AlertDialog alert2 = builder2.create();
+                                alert2.show();
+
+                            }
+                        })
+                        .setNegativeButton("Hayır", null);
+
+                        AlertDialog alert = builder1.create();
+                        alert.show();
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(++check > 1) {
+            if (parent.getItemAtPosition(position).toString().equals("TamBugday"))
+                TamBugday++;
+            else if (parent.getItemAtPosition(position).toString().equals("Cavdar"))
+                Cavdar++;
+            else if (parent.getItemAtPosition(position).toString().equals("Kepekli"))
+                Kepekli++;
+            else if (parent.getItemAtPosition(position).toString().equals("BeyazEkmek"))
+                BeyazEkmek++;
+
+                text1 = parent.getItemAtPosition(position).toString() + " Sepete Eklendi.";
+                text += parent.getItemAtPosition(position).toString() + "\n";
+                Toast.makeText(parent.getContext(), text1, Toast.LENGTH_SHORT).show();
+
+
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
