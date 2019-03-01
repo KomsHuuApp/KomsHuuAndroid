255cec23976a8ccc566dd3d2dcf766c96f179def
irmakersevinc <irmakersevinc@gmail.com>
2019-03-01 21:25:00 +0300

    Yöneticinin şikayetleri silebileceği ekranın temelleri.

--- app/src/main/java/com/komshuu/komshuuandroidfrontend/adapters/ComplaintAdapter.java
+++ app/src/main/java/com/komshuu/komshuuandroidfrontend/adapters/ComplaintAdapter.java
@@ -1,6 +1,8 @@
 package com.komshuu.komshuuandroidfrontend.adapters;
 
 import android.content.Context;
+import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +10,20 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.komshuu.komshuuandroidfrontend.R;
+import com.komshuu.komshuuandroidfrontend.UserOrderActivity;
 import com.komshuu.komshuuandroidfrontend.models.Complaint;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ComplaintAdapter extends RecyclerView.Adapter<ComplaintAdapter.MyViewHolder> {
@@ -35,6 +48,16 @@ public class ComplaintAdapter extends RecyclerView.Adapter<ComplaintAdapter.MyVi
     public void onBindViewHolder(ComplaintAdapter.MyViewHolder holder, final int position) {
         Complaint selectedProduct = mComplaintList.get(position);
         holder.setData(selectedProduct, position);
+
+        holder.deleteproduct.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Complaint theRemovedItem = mComplaintList.get(position);
+
+                // remove your item from data base
+                mComplaintList.remove(position);  // remove the item from list
+                notifyItemRemoved(position); // notify the adapter about the removed item
+            }
+        });
     }
 
     @Override
@@ -53,8 +76,6 @@ public class ComplaintAdapter extends RecyclerView.Adapter<ComplaintAdapter.MyVi
             complaintDate = (TextView) itemView.findViewById(R.id.complaintDate);
             complaintDescription = (TextView) itemView.findViewById(R.id.complaintDescription);
             deleteproduct = (ImageView) itemView.findViewById(R.id.deleteproduct);
-            deleteproduct.setOnClickListener(this);
-
         }
 
         public void setData(Complaint selectedProduct, int position) {
