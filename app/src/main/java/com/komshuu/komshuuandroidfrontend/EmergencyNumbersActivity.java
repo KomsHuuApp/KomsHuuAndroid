9dd78144ca44eaabd2277e4fa4490f1d7c7a4057
Servet Ozboke <sozboke@etu.edu.tr>
2019-03-09 18:56:18 +0300

    Emergency Numbers screen administrator delete issue solved

--- app/src/main/java/com/komshuu/komshuuandroidfrontend/EmergencyNumbersActivity.java
+++ app/src/main/java/com/komshuu/komshuuandroidfrontend/EmergencyNumbersActivity.java
@@ -1,13 +1,9 @@
 package com.komshuu.komshuuandroidfrontend;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -17,19 +13,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.komshuu.komshuuandroidfrontend.adapters.EmergencyCallNumberAdapter;
-import com.komshuu.komshuuandroidfrontend.models.Apartment;
 import com.komshuu.komshuuandroidfrontend.models.EmergencyCallNumber;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class EmergencyNumbersActivity extends AppCompatActivity {
@@ -81,38 +68,5 @@ public class EmergencyNumbersActivity extends AppCompatActivity {
         });
         queue.add(stringRequest);
 
-    }
-
-    public void delete(View v) {
-        TextView textView2 = (TextView) findViewById(R.id.emergencyname);
-        TextView textView = (TextView) findViewById(R.id.emergencyId);
-        TextView textView1 = (TextView) findViewById(R.id.apartmentId);
-        ImageView imageView = (ImageView) findViewById(R.id.deleteproduct);
-        System.out.println(textView2.getText());
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://enigmatic-atoll-89666.herokuapp.com/deleteEmergencyNumber?id=" + textView.getText() + "&apartmentId=" + textView1.getText();
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        emergencyCallNumbers = new ArrayList<>();
-                        try {
-                            if (response.equals("true")) {
-                                CardView cardView = (CardView) findViewById(R.id.cardView);
-                                cardView.setVisibility(View.GONE);
-                                finish();
-                                startActivity(getIntent());
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        queue.add(stringRequest);
     }
 }
--- app/src/main/java/com/komshuu/komshuuandroidfrontend/adapters/EmergencyCallNumberAdapter.java
+++ app/src/main/java/com/komshuu/komshuuandroidfrontend/adapters/EmergencyCallNumberAdapter.java
@@ -1,24 +1,36 @@
 package com.komshuu.komshuuandroidfrontend.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.komshuu.komshuuandroidfrontend.models.EmergencyCallNumber;
 import com.komshuu.komshuuandroidfrontend.R;
+
 import java.util.ArrayList;
 
 public class EmergencyCallNumberAdapter extends RecyclerView.Adapter<EmergencyCallNumberAdapter.MyViewHolder> {
 
     ArrayList<EmergencyCallNumber> mEmergencyCallNumberList;
     LayoutInflater inflater;
+    Context context;
 
     public EmergencyCallNumberAdapter(Context context, ArrayList<EmergencyCallNumber> product) {
         inflater = LayoutInflater.from(context);
         this.mEmergencyCallNumberList = product;
+        this.context = context;
     }
 
     @Override
@@ -53,7 +65,9 @@ public class EmergencyCallNumberAdapter extends RecyclerView.Adapter<EmergencyCa
             apartmentId = (TextView) itemView.findViewById(R.id.apartmentId);
             emergencyCallImage = (ImageView) itemView.findViewById(R.id.emergencyImage);
             deleteProduct = (ImageView) itemView.findViewById(R.id.deleteproduct);
+            deleteProduct.setOnClickListener(this);
             editProduct = (ImageView) itemView.findViewById(R.id.editproduct);
+            editProduct.setOnClickListener(this);
         }
 
         public void setData(EmergencyCallNumber selectedProduct, int position) {
@@ -67,10 +81,37 @@ public class EmergencyCallNumberAdapter extends RecyclerView.Adapter<EmergencyCa
 
         @Override
         public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Are you sure?");
+            builder.setMessage("After this process, the deleted data can not be retrieve");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    RequestQueue queue = Volley.newRequestQueue(context);
+                    System.out.println(emergencyId.getText() + " " + emergencyCallName.getText());
+                    String url = "https://enigmatic-atoll-89666.herokuapp.com/deleteEmergencyNumber?id=" + emergencyId.getText() + "&apartmentId=" + apartmentId.getText();
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
 
+                        }
+                    });
+                    queue.add(stringRequest);
+                    mEmergencyCallNumberList.remove(getPosition());
+                    notifyItemRemoved(getPosition());
+                    notifyItemRangeChanged(getPosition(), mEmergencyCallNumberList.size());
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
 
+                }
+            });
+            builder.show();
         }
-
-
     }
 }
