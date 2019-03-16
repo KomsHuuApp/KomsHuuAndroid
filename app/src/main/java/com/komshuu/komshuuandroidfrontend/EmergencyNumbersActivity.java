d830aa6494a59185048f2805c7f92b80c23cffaf
Servet Ozboke <sozboke@etu.edu.tr>
2019-03-16 16:59:38 +0300

    Adding Emergency Call Numbers screen finished

--- app/src/main/java/com/komshuu/komshuuandroidfrontend/EmergencyNumbersActivity.java
+++ app/src/main/java/com/komshuu/komshuuandroidfrontend/EmergencyNumbersActivity.java
@@ -1,15 +1,26 @@
 package com.komshuu.komshuuandroidfrontend;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.komshuu.komshuuandroidfrontend.adapters.EmergencyCallNumberAdapter;
@@ -68,5 +79,61 @@ public class EmergencyNumbersActivity extends AppCompatActivity {
         });
         queue.add(stringRequest);
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.emergency_number_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if (id == R.id.action_new_emergency) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            View view = getLayoutInflater().inflate(R.layout.emergency_add_layout, null);
+            final EditText editTextEmergencyDescription = (EditText) view.findViewById(R.id.add_emergency_description);
+            final EditText editTextEmergencyNumber = (EditText) view.findViewById(R.id.add_emergency_number);
+            builder.setView(view).setTitle("Yeni Çağrı Numarası").setNegativeButton("İptal", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            }).setPositiveButton("Ekle", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    RequestQueue queue = Volley.newRequestQueue(EmergencyNumbersActivity.this);
+                    String url = "https://enigmatic-atoll-89666.herokuapp.com/addEmergencyCallNumber";
+                    JSONObject jsonObject = new JSONObject();
+                    try {
+                        jsonObject.put("name", editTextEmergencyDescription.getText());
+                        jsonObject.put("phoneNumber", editTextEmergencyNumber.getText());
+                        jsonObject.put("apartmentId", 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(EmergencyNumbersActivity.this, "Çağrı Numarası paylaşıldı.", Toast.LENGTH_LONG).show();
+                            recreate();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(EmergencyNumbersActivity.this, "Çağrı Numarası paylaşılamadı!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    queue.add(jsonObjectRequest);
+                }
+            });
+            AlertDialog addDialog = builder.create();
+            addDialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
